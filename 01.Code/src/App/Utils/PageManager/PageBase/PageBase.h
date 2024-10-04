fa766c32997d1252f4b2d79029f3c4392521ae3b
JCTsai0 <JCTsai0@winbond.com>
2024-10-04 13:48:53 +0800

    Push code for add some function in PageManager header file.

--- 01.Code/src/App/Utils/PageManager/PageBase/PageBase.h
+++ 01.Code/src/App/Utils/PageManager/PageBase/PageBase.h
@@ -3,12 +3,128 @@
 
 #include "lvgl.h"
 
-//Class
-//Define Function
+// Class
+// Define macro on Header file
+///@brief Generate stash area
+/*Example
+    struct PageStash {
+        void* data;
+        size_t size;
+    };
+    int myData = 42;
+    PageStash stash = PAGE_STASH_MAKE(myData);
+    std::cout << "Data address: " << stash.data << std::endl;
+    //Data address: 0x7ffee4c8a5ac
+    std::cout << "Data size: " << stash.size << " bytes" << std::endl;
+    //Data size: 4 bytes
+*/
+#define PAGE_STASH_MAKE(data) {&(data), sizeof(data)}
+
+///@brief Get the data from stash area
+#define PAGE_STASH_GET(data) {this->GetStash(&(data), sizeof(data))}
+
+/// @brief Define PageManager class
+class PageManager;
 
 class PageBase {
     public:
+        /// @brief  Page status
+        enum PAGESTATUS_t {
+            PAGE_STATE_IDLE,
+            PAGE_STATE_LOAD,
+            PAGE_STATE_WILL_APPEAR,
+            PAGE_STATE_DID_APPEAR,
+            PAGE_STATE_ACTIVITY,
+            PAGE_STATE_WILL_DISAPPEAR,
+            PAGE_STATE_DID_DISAPPEAR,
+            PAGE_STATE_UNLOAD,
+            /* Sentinel value indicating the last valid pages status type */
+            _PAGE_STATE_LAST = PAGE_STATE_UNLOAD
+        };
 
-    private:
+        /// @brief Page stash area structure
+        struct PAGESTASH_t {
+                void* ptrPage;
+                uint32_t size;
+        };
+
+        /// @brief Page switching animation propertiess
+        struct ANIMATTR_t {
+                uint8_t animType;
+                uint8_t animTime;
+                lv_anim_path_cb_t lvAnimPath;
+        };
+
+    public:
+        ///@brief UI root node pointer
+        lv_obj_t* lvRoot;
+        ///@brief Page manager pointer
+        PageManager* pageManager;
+        ///@brief Page name
+        const char* pageName;
+        ///@brief Page ID
+        uint16_t pageID;
+        ///@brief Page user data pointer
+        void* pageUserData;
+
+        ///@brief Private data, Only page manager access
+        struct {
+                bool ReqEnableCache;
+                bool ReqDisableAutoCache;
+                bool IsDisableAutoCache;
+                bool IsCached;
+                PAGESTATUS_t PageStatus;
+                PAGESTASH_t pageStash;
+                struct {
+                        bool isEnter;
+                        bool IsBusy;
+                        ANIMATTR_t animAttr;
+                } PageAnimInfo;
+        } pageManagerPrivate;
+
+    public:
+        virtual ~PageBase() {}
+        /* Page load */
+        virtual void onViewLoad() {}
+        /* Page load complete */
+        virtual void onViewDidLoad() {}
+        /* Page will be displayed soon  */
+        virtual void onViewWillAppear() {}
+        /* The page is displayed  */
+        virtual void onViewDidAppear() {}
+        /* Page is about to disappear */
+        virtual void onViewWillDisappear() {}
+        /* Page disappeared complete  */
+        virtual void onViewDidDisappear() {}
+        /* Page uninstall complete  */
+        virtual void onViewDidUnload() {}
+        /* Set whether to manually manage the cache */
+        void SetCustomCacheEnable(bool en) {
+            SetCustomAutoCacheEnable(false);
+            pageManagerPrivate.ReqEnableCache = en;
+        }
+        /* Set whether to enable automatic cache */
+        void SetCustomAutoCacheEnable(bool en) {
+            pageManagerPrivate.ReqDisableAutoCache = !en;
+        }
+        /* Set custom animation properties  */
+        void SetCustomLoadAnimType(
+            uint8_t animType, uint16_t animTime = 500,
+            lv_anim_path_cb_t path = lv_anim_path_ease_out) {
+            pageManagerPrivate.PageAnimInfo.animAttr.animType = animType;
+            pageManagerPrivate.PageAnimInfo.animAttr.animTime = animTime;
+            pageManagerPrivate.PageAnimInfo.animAttr.lvAnimPath = path;
+        }
+
+        bool GetStash(void* willAppearPage, uint32_t ptrSize) {
+            bool result = false;
+            if (pageManagerPrivate.pageStash.ptrPage != nullptr &&
+                pageManagerPrivate.pageStash.size == ptrSize) {
+                memcpy(willAppearPage, pageManagerPrivate.pageStash.ptrPage,
+                       pageManagerPrivate.pageStash.size);
+                result = true;
+            }
+            return result;
+        }
 };
 #endif
--- 01.Code/src/App/Utils/PageManager/PageManager.h
+++ 01.Code/src/App/Utils/PageManager/PageManager.h
@@ -1,20 +1,151 @@
 #ifndef __PAGE_MANAGER_H
 #define __PAGE_MANAGER_H
-class PageManager
-{
-private:
-    /* data */
-public:
-    PageManager(/* args */);
-    ~PageManager();
+
+#include <stack>
+#include <vector>
+
+#include "PageBase.h"
+#include "PageFactory.h"
+
+class PageManager {
+    public:
+        /// @brief Animated setter
+        typedef lv_anim_exec_xcb_t lv_anim_setter_t;
+        /// @brief Animated getter
+        typedef lv_coord_t (*lv_anim_getter_t)(void*);
+
+    public:
+        /// @brief Page switching animation type
+        enum SWITCHANIMATIONTYPE_t {
+            /* Default (global) animation type  */
+            LOAD_ANIM_GLOBAL = 0,
+            /* New page overwrites old page  */
+            LOAD_ANIM_OVER_LEFT,
+            LOAD_ANIM_OVER_RIGHT,
+            LOAD_ANIM_OVER_TOP,
+            LOAD_ANIM_OVER_BOTTOM,
+            /* New page pushes old page  */
+            LOAD_ANIM_MOVE_LEFT,
+            LOAD_ANIM_MOVE_RIGHT,
+            LOAD_ANIM_MOVE_TOP,
+            LOAD_ANIM_MOVE_BOTTOM,
+            /* The new interface fades in, the old page fades out */
+            LOAD_ANIM_FADE_ON,
+            /* No animation */
+            LOAD_ANIM_NONE,
+            /* Sentinel value indicating the last valid animation type */
+            _LOAD_ANIM_LAST = LOAD_ANIM_NONE
+        };
+        /// @brief Page dragging direction
+        enum ROOTDRAGDIRTYPE_t {
+            ROOT_DRAG_NONE,
+            ROOT_DRAG_HOR,
+            ROOT_DRAG_VER,
+            ROOT_DRAG_ALL,
+            _ROOT_DRAG_LAST = ROOT_DRAG_ALL
+
+        };
+        /// @brief Page switching animation properties
+        struct ANIMVALUE_t {
+                /// @brief As the entered party
+                struct {
+                        lv_coord_t start;
+                        lv_coord_t end;
+                } ENTER;
+                /// @brief As the exited party
+                struct {
+                        lv_coord_t start;
+                        lv_coord_t end;
+                } EXIT;
+        };
+        /// @brief Page loading animation properties
+        struct LOADANIMATTR_t {
+                lv_anim_setter_t setter;
+                lv_anim_getter_t getter;
+                ROOTDRAGDIRTYPE_t dragDir;
+                ANIMVALUE_t push;
+                ANIMVALUE_t pop;
+        };
+
+    public:
+        PageManager(PageFactory* pageFactory = nullptr);
+        ~PageManager();
+
+        // Page Loader
+        /// @brief Page install
+        PageBase* InstallPage(const char* className, const char* pageName);
+        /// @brief Page uninstall
+        bool UninstallPage(const char* pageName);
+        /// @brief Page load
+        bool Register(PageBase* pageBase, const char* pageName);
+        /// @brief Page unload
+        bool Unregister(const char* pageName);
+
+        // Page Router
+        PageBase* Push(const char* pageName,
+                       const PageBase::PAGESTASH_t* pageStash = nullptr);
+        PageBase* Pop();
+        bool BackHome();
+        const char* GetPagePrevName();
+
+        // Global Page Animation
+        void SetGlobalPageAnimation(
+            SWITCHANIMATIONTYPE_t anim = LOAD_ANIM_OVER_LEFT,
+            uint16_t time = 500,
+            lv_anim_path_cb_t path = lv_anim_path_ease_out);
+
+    private:
+        PageFactory* pageFactory;
+        PageBase* pageCurrent;
+        PageBase* pagePrev;
+        std::vector<PageBase*> pagePool;
+        std::stack<PageBase*> pageStack;
+
+        struct {
+                bool IsSwitchReq; // Whether to switch request
+                bool IsBusy;      // Is switching
+                bool IsPushing;   // Whether it is in push state
+
+                PageBase::ANIMATTR_t Current; // Current animation properties
+                PageBase::ANIMATTR_t Global;  // Global animation properties
+        } animationState;
+
+    private:
+        // About Page Pool
+        /// @brief  Find the page in the page pool
+        /// @param pageName
+        /// @return PageBase*
+        PageBase* FindPageInPool(const char* pageName);
+
+        // About Page Stack
+        /// @brief  Find the page in the page stack
+        /// @param pageName
+        /// @return PageBase*
+        PageBase* FindPageInStack(const char* pageName);
+        PageBase* GetPageStackTop();
+        PageBase* GetPageStackTopAfter();
+        void SetPageStackClear(bool keepToBottom = true);
+        bool FourceUnload(PageBase* pageBase);
+
+        // About Page Animation
+        bool GetLoadAnimAttr(uint8_t anim, LOADANIMATTR_t* loadAnimAttr);
+        bool GetIsOverAnim(uint8_t anim) {
+            return (anim >= LOAD_ANIM_OVER_LEFT &&
+                    anim <= LOAD_ANIM_OVER_BOTTOM);
+        }
+        bool GetIsMoveAnim(uint8_t anim) {
+            return (anim >= LOAD_ANIM_MOVE_LEFT &&
+                    anim <= LOAD_ANIM_MOVE_BOTTOM);
+        }
+        void AnimDefaultInit(lv_anim_t* a);
+        bool GetCurrentLoadAnimAttr(LOADANIMATTR_t* attr) {
+            return GetLoadAnimAttr(GetCurrentLoadAnimType(), attr);
+        }
+        SWITCHANIMATIONTYPE_t GetCurrentLoadAnimType() {
+            return (SWITCHANIMATIONTYPE_t)animationState.Current.animType;
+        }
+
+        //About Page Root
+        
 };
-
-PageManager::PageManager(/* args */)
-{
-}
-
-PageManager::~PageManager()
-{
-}
-
 #endif
